d5afa1ae4cbbe618040f17c6256e373ef78d7b0c
Lorenzo Milesi <lorenzo.milesi@live.fr>
2020-10-02 22:58:05 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Java FizzBuzz Code Kata with TDD
+
+This Code Kata is inspired from [https://codingdojo.org/kata/FizzBuzz/](https://codingdojo.org/kata/FizzBuzz/)
+and is written in Java with TDD.
---
+++ src/com/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,11 @@
+package com.fizzbuzz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzBuzzTest {
+    @Test
+    public void doesNothing() {
+    }
+}
