41fd4f8b1ebe711f9619233c687f27f13c15aeb9
Lorenzo Milesi <lorenzo.milesi@live.fr>
2020-10-02 23:10:43 +0200

    [Refactoring] Tests still pass
    Signed-off-by: Lorenzo Milesi <lorenzo.milesi@live.fr>

--- src/com/fizzbuzz/FizzBuzz.java
+++ src/com/fizzbuzz/FizzBuzz.java
@@ -2,9 +2,6 @@ package com.fizzbuzz;
 
 public class FizzBuzz {
     public String generate(int number) {
-        if (number == 1) {
-            return "1";
-        }
-        return "2";
+        return String.valueOf(number);
     }
 }
