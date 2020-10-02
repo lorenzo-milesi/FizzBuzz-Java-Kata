f906b94419c757c692b07b0a6338b0f5d0066179
Lorenzo Milesi <lorenzo.milesi@live.fr>
2020-10-02 23:16:30 +0200

    [Test]When 6, return "Fizz" Alright, silly examples :D
    Signed-off-by: Lorenzo Milesi <lorenzo.milesi@live.fr>

--- src/com/fizzbuzz/FizzBuzz.java
+++ src/com/fizzbuzz/FizzBuzz.java
@@ -2,7 +2,7 @@ package com.fizzbuzz;
 
 public class FizzBuzz {
     public String generate(int number) {
-        if (3 == number) {
+        if (3 == number || 6 == number) {
             return "Fizz";
         }
         return String.valueOf(number);
--- src/com/fizzbuzz/FizzBuzzTest.java
+++ src/com/fizzbuzz/FizzBuzzTest.java
@@ -1,23 +1,35 @@
 package com.fizzbuzz;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FizzBuzzTest {
+    private FizzBuzz _fizzBuzz;
+
+    @BeforeEach
+    public void setUp() {
+        _fizzBuzz = new FizzBuzz();
+    }
 
     @Test
     public void shouldReturn1When1() {
-        assertEquals("1", new FizzBuzz().generate(1));
+        assertEquals("1", _fizzBuzz.generate(1));
     }
 
     @Test
     public void shouldReturn2When2() {
-        assertEquals("2", new FizzBuzz().generate(2));
+        assertEquals("2", _fizzBuzz.generate(2));
     }
 
     @Test
     public void shouldReturnFizzWhen3() {
-        assertEquals("Fizz", new FizzBuzz().generate(3));
+        assertEquals("Fizz", _fizzBuzz.generate(3));
+    }
+
+    @Test
+    public void shouldReturnFizzWhen6() {
+        assertEquals("Fizz", _fizzBuzz.generate(6));
     }
 }
