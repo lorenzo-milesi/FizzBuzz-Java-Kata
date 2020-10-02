110c4e914b591aacf425fd0d060c0afc87caa6c4
Lorenzo Milesi <lorenzo.milesi@live.fr>
2020-10-02 23:13:14 +0200

    [Test]When 3, return "Fizz"
    Signed-off-by: Lorenzo Milesi <lorenzo.milesi@live.fr>

--- src/com/fizzbuzz/FizzBuzz.java
+++ src/com/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,9 @@ package com.fizzbuzz;
 
 public class FizzBuzz {
     public String generate(int number) {
+        if (3 == number) {
+            return "Fizz";
+        }
         return String.valueOf(number);
     }
 }
--- src/com/fizzbuzz/FizzBuzzTest.java
+++ src/com/fizzbuzz/FizzBuzzTest.java
@@ -7,12 +7,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class FizzBuzzTest {
 
     @Test
-    public void shouldReturn1() {
+    public void shouldReturn1When1() {
         assertEquals("1", new FizzBuzz().generate(1));
     }
 
     @Test
-    public void shoudReturn2() {
+    public void shouldReturn2When2() {
         assertEquals("2", new FizzBuzz().generate(2));
+    }
+
+    @Test
+    public void shouldReturnFizzWhen3() {
+        assertEquals("Fizz", new FizzBuzz().generate(3));
     }
 }
