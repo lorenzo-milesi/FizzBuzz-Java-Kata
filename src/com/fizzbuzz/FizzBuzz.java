8e497f9cca9980f6ccbb08af50519c0e59ba62a4
Lorenzo Milesi <lorenzo.milesi@live.fr>
2020-10-02 23:20:27 +0200

    Test: Multiples of 3 should return Fizz
    Signed-off-by: Lorenzo Milesi <lorenzo.milesi@live.fr>

--- src/com/fizzbuzz/FizzBuzz.java
+++ src/com/fizzbuzz/FizzBuzz.java
@@ -2,7 +2,7 @@ package com.fizzbuzz;
 
 public class FizzBuzz {
     public String generate(int number) {
-        if (3 == number || 6 == number) {
+        if (0 == number % 3) {
             return "Fizz";
         }
         return String.valueOf(number);
